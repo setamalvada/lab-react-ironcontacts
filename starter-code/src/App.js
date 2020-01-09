eb7219caf190e7560c88def154e62c2da5ffe386
Julio García Pérez <julio.garciaperez@telefonica.com>
2020-01-09 21:09:48 +0100

    refactor

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,21 +4,20 @@ import ContactList from './components/contacts/ContactList';
 import contacts from './data/contacts.json';
 import Header from './components/misc/Header';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <div className="container mt-4">
-          <div className="row">
-            <div className="col-12">
-              <ContactList contacts={contacts} />
-            </div>
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-12">
+            <ContactList contacts={contacts} />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
--- starter-code/src/components/contacts/ContactRow.js
+++ starter-code/src/components/contacts/ContactRow.js
@@ -1,13 +1,22 @@
 
 import React from 'react';
 
-const ContactRow = (props) => {
+const ContactRow = ({ pictureUrl, name, popularity, onClickDelete }) => {
   return (
     <tr>
-      <td><img src={props.pictureUrl} alt={props.name} className="img-thumbnail" style={{ maxWidth: "70px" }}/></td>
-      <td>{props.name}</td>
-      <td>{props.popularity.toFixed(2)}</td>
-      <td><button className="btn btn-danger" onClick={props.onClickDelete}>x</button></td>
+      <td>
+        <img
+          src={pictureUrl}
+          alt={name}
+          className="img-thumbnail"
+          style={{ maxWidth: "70px" }}
+        />
+      </td>
+      <td>{name}</td>
+      <td>{popularity.toFixed(2)}</td>
+      <td>
+        <button className="btn btn-danger" onClick={onClickDelete}>x</button>
+      </td>
     </tr>
   )
 }
